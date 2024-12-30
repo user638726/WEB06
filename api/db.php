ef3db1253d30cc0ad65010b493c145eba496337d
user638726 <lienjackson35@gmail.com>
2024-12-30 16:09:31 +0800

    新增api裡的php

--- api/db.php
+++ api/db.php
@@ -161,9 +161,8 @@ function to($url){
     header("location:".$url);
 }
 
-
 $Total=new DB('total');
-
+$User=new DB('users');
 
 if(!isset($_SESSION['view'])){
     if($Total->count(['date'=>date("Y-m-d")])>0){
