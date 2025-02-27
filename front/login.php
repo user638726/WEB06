139bd4a59cd0f6baf41450eb579e67b2679375ca
user638726 <lienjackson35@gmail.com>
2025-02-27 14:09:03 +0800

    增加樣式

--- front/login.php
+++ front/login.php
@@ -15,7 +15,7 @@
                 <input type="reset" value="清除" onclick="resetForm()">
             </td>
             <td>
-                <a href="?do=forgot">忘記密碼</a>
+                <a href="?do=forgot">忘記密碼|</a>
                 <a href="?do=reg">尚未註冊</a>
             </td>
         </tr>
@@ -23,39 +23,40 @@
 </fieldset>
 
 <script>
-
-function login(){
-    let user={
-        acc:$("#acc").val(),
-        pw:$("#pw").val(),
+function login() {
+    let user = {
+        acc: $("#acc").val(),
+        pw: $("#pw").val(),
     }
-    
-        $.get("./api/chk_acc.php",{acc:user.acc},(res)=>{
-            console.log("chk acc => ",res)
-            if(parseInt(res)==0){
-                alert("查無帳號")
-                resetForm()
-            }else{
-                $.post("./api/chk_pw.php",user,(res)=>{
-                    console.log("login => ",res)
-                    if(parseInt(res)==1){
-                        if(user.acc=='admin'){
-                            location.href='admin.php';
-                        }else{
-                            location.href='index.php';
-                        }
-                    }else{
-                        alert("密碼錯誤")
-                        resetForm()
+
+    $.get("./api/chk_acc.php", {
+        acc: user.acc
+    }, (res) => {
+        console.log("chk acc => ", res)
+        if (parseInt(res) == 0) {
+            alert("查無帳號")
+            resetForm()
+        } else {
+            $.post("./api/chk_pw.php", user, (res) => {
+                console.log("login => ", res)
+                if (parseInt(res) == 1) {
+                    if (user.acc == 'admin') {
+                        location.href = 'admin.php';
+                    } else {
+                        location.href = 'index.php';
                     }
-                })
-            }
-        })
-    
+                } else {
+                    alert("密碼錯誤")
+                    resetForm()
+                }
+            })
+        }
+    })
+
 }
 
-function resetForm(){
+function resetForm() {
     $("#acc").val("")
     $("#pw").val("")
-}    
+}
 </script>
